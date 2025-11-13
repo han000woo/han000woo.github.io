63911882b1aca83e9820e7db38bb1c5bd3d56a30
한선우 <gkstjsdn0011@naver.com>
2025-11-13 22:28:44 +0900

    vite 수정

--- cote-repo-monitoring/vite.config.js
+++ cote-repo-monitoring/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: './cote-repo-monitoring/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
